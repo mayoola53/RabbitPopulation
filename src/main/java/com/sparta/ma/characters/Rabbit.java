d6839851a2446e9ba1b67529fc722c02377e8a29
Michael Ayoola <mayoola53@yahoo.co.uk>
2019-11-06 16:41:57 +0000

    Added methods and member variables to my rabbits

--- src/main/java/com/sparta/ma/characters/Rabbit.java
+++ src/main/java/com/sparta/ma/characters/Rabbit.java
@@ -1,4 +1,29 @@
 package com.sparta.ma.characters;
 
 public class Rabbit {
+
+    private int age;
+    private boolean gender;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+
+
+
+
+
 }
---
+++ src/main/java/com/sparta/ma/charactersController/SimulationEnviorment.java
@@ -0,0 +1,4 @@
+package com.sparta.ma.charactersController;
+
+public class SimulationEnviorment {
+}
