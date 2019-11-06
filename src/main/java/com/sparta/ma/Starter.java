1531fa8545be7fa1e957da389bd54d5dfd8f5458
Michael Ayoola <mayoola53@yahoo.co.uk>
2019-11-06 16:32:17 +0000

    initial commit

---
+++ src/main/java/com/sparta/ma/Starter.java
@@ -0,0 +1,15 @@
+package com.sparta.ma;
+
+/**
+ * Hello world!
+ *
+ */
+public class Starter
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
---
+++ src/main/java/com/sparta/ma/characters/Rabbit.java
@@ -0,0 +1,4 @@
+package com.sparta.ma.characters;
+
+public class Rabbit {
+}
